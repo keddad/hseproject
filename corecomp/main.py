312d7c2beda42ac9e8f0644eaef59680aebdf481
keddad <keddad@yandex.ru>
2020-05-02 22:00:46 +0300

    CoreComp: Implemented /recface/

--- corecomp/main.py
+++ corecomp/main.py
@@ -4,6 +4,7 @@ from utils.redis import connect_to_redis, close_redis_connection
 import logging
 from time import sleep
 from addface.routing import addface_router
+from recface.routing import recface_router
 
 app = FastAPI()
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)-15s %(message)s")
@@ -22,9 +23,13 @@ app.add_event_handler("shutdown", close_mongo_connection)
 app.add_event_handler("startup", connect_to_redis)
 app.add_event_handler("shutdown", close_redis_connection)
 
-
 app.include_router(
     addface_router,
+    prefix="/task"
+)
+
+app.include_router(
+    recface_router,
     prefix="/task"
 )
 
---
+++ corecomp/recface/structs.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from utils.structs import Trait, TaskState
+
+
+class FaceRecRequest(BaseModel):
+    face: str
+
+
+class PersonInformation(BaseModel):
+    probability: float
+    traits: List
+
+
+class NewTaskToRedis(BaseModel):
+    task_id: str
+
+
+class FaceRecResult(BaseModel):
+    state: TaskState
+    message: str = ""
+    matches: Optional[List[PersonInformation]]
