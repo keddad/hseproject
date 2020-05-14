b8ca48ba6a7f793dafc425bba25cbf231704f582
keddad <keddad@yandex.ru>
2020-05-15 01:06:15 +0300

    Corecomp: Implemented social.check_parsetask

--- corecomp/social/routing.py
+++ corecomp/social/routing.py
@@ -21,3 +21,22 @@ async def new_parsetask(network: SocialNetwork, page_id: str, skip_existing: boo
                                         append_face=(1 if skip_existing else 0)).dict())
 
     return task_id
+
+
+@social_router.post("/{task_id}", response_model=DefaultResult)
+async def check_parsetask(task_id: str):
+    redis_client = await get_redis_client()
+
+    if await redis_client.exists(task_id) == 0:
+        raise HTTPException(status_code=404, detail="No such task")
+
+    result = await redis_client.get(task_id)
+
+    res_arr = result.decode().split()
+    res_state = TaskState(res_arr[0])
+
+    if res_state in (TaskState.ok, TaskState.failed):
+        await redis_client.dump(task_id)
+        logging.debug(f"Dumped {task_id}")
+
+    return DefaultResult(state=res_state, message=res_arr[1] if len(res_arr) > 1 else "")
