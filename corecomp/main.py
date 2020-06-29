a3d5a90c49ece589fd87f63faa7dc295b95e64e5
keddad <keddad@yandex.ru>
2020-06-29 18:01:52 +0300

    Removed old version of corecomp

--- corecomp/main.py
+++ corecomp/main.py
@@ -1,38 +1,10 @@
+from typing import Optional
+
 from fastapi import FastAPI
-from utils.mongodb import connect_to_mongo, close_mongo_connection
-from utils.redis import connect_to_redis, close_redis_connection
-import logging
-from time import sleep
-from addface.routing import addface_router
-from recface.routing import recface_router
-from social.routing import social_router
 
 app = FastAPI()
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)-15s %(message)s")
 
 
-@app.get("/isalive")
+@app.get("/api/home")
 def read_root():
-    return "ok"
-
-
-app.add_event_handler("startup", connect_to_mongo)
-app.add_event_handler("shutdown", close_mongo_connection)
-
-app.add_event_handler("startup", connect_to_redis)
-app.add_event_handler("shutdown", close_redis_connection)
-
-app.include_router(
-    addface_router,
-    prefix="/api/task"
-)
-
-app.include_router(
-    recface_router,
-    prefix="/api/task"
-)
-
-app.include_router(
-    social_router,
-    prefix="/api/parse"
-)
+    return {"Hello": "World"}
